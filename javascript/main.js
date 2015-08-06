f8f24f149dcada1ecb8d8778de8431c80bf09133
hhassan1230 <hhassan@oswego.edu>
2015-08-06 15:40:38 -0400

    fixed js logic in ham

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,9 @@ $("document").ready(function(){
   $(".moveTo").on("click", function(){
     if ($(".c-hamburger--rot").hasClass('is-active')) {
       $(hamDiv).removeClass("is-active");
-      $('.nav-list').hide();
+      if ($(window).width() < 750) {
+        $('.nav-list').hide();
+      };
     };
     var clicked = $(this).attr('href');
     $('html, body').animate({
@@ -17,16 +19,24 @@ $("document").ready(function(){
         (this.classList.contains("is-active") === true) ? $('.nav-list').show() : $('.nav-list').hide();
       })
   };
-  // $( window ).resize(function() {
-  //     $('.nav-list').hide();
-  //   if ($(window).width() < 750){
-  //     $(hamDiv).on('click', function(){
-  //       (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-  //       (this.classList.contains("is-active") === true) ? $('.nav-list').show() : $('.nav-list').hide();
-  //     })
-  //   } else {
-  //     // $(hamDiv).classList.remove("is-active");
-  //     $('.nav-list').show();
-  //   };
-  // });
+  $( window ).resize(function() {
+    var times = 0;
+    var hamDiv = document.getElementById('hamburger');
+      if (hamDiv.classList.contains("is-active") === true) {
+        hamDiv.classList.remove("is-active");
+        $('.nav-list').hide(); 
+      };
+
+      if (($('.nav-list').is(':visible'))) {
+        hamDiv.classList.add("is-active");
+      };
+      // if the nav-list doesn't show when ham is active
+      if (($(window).width() < 750) && (hamDiv.classList.contains("is-active") === true)){
+        $('.nav-list').show(); 
+      };  
+      // show nav in desktop
+      if ($(window).width() > 750) {
+        $('.nav-list').show(); 
+      };
+  });
 })
