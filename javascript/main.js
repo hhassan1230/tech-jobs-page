6fe69daf0e1b64a7bfc6dba98431fd2aa261e474
hhassan1230 <hhassan@oswego.edu>
2015-08-07 17:51:52 -0400

    fixes and refactors ham

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,48 @@
 $("document").ready(function(){
-  var hamDiv = document.getElementById('hamburger');
-  $(".moveTo").on("click", function(){
+  if ($(window).width() < 750){
+    $("#hamburger").on('click', function(){        
+      if (this.classList.contains("is-active")) {
+        this.classList.remove("is-active");
+        $('.nav-list').hide();
+      } else {
+        this.classList.add("is-active");
+        $('.nav-list').show();
+      };
+    })
+    $(".moveTo").on("click", function(){
+      checkHam();
+      var clicked = $(this).attr('href');
+      scrolling(clicked);
+      return false;
+    });
+  } else {
+    $(".moveTo").on("click", function(){
+      var clicked = $(this).attr('href');
+      scrolling(clicked);
+      return false;
+    });
+  };
+
+  $( window ).resize(function() {
+    if ($(window).width() < 750) {
+      $('.nav-list').hide(); 
+      $("#hamburger").removeClass("is-active");
+    } else{
+      $('.nav-list').show(); 
+    };
+  });
+
+  function scrolling(clicked) {
+    $('html, body').animate({
+        scrollTop: $(clicked).offset().top - 35
+      }, 1000);
+  }
+  function checkHam() {
     if ($(".c-hamburger--rot").hasClass('is-active')) {
-      $(hamDiv).removeClass("is-active");
+      $('#hamburger').removeClass("is-active");
       if ($(window).width() < 750) {
         $('.nav-list').hide();
       };
     };
-    var clicked = $(this).attr('href');
-    $('html, body').animate({
-          scrollTop: $(clicked).offset().top - 35
-      }, 1000);
-    return false;
-    });
-  if ($(window).width() < 750){
-      $(hamDiv).on('click', function(){
-        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-        (this.classList.contains("is-active") === true) ? $('.nav-list').show() : $('.nav-list').hide();
-      })
-  };
-  $( window ).resize(function() {
-    var times = 0;
-    var hamDiv = document.getElementById('hamburger');
-      if (hamDiv.classList.contains("is-active") === true) {
-        hamDiv.classList.remove("is-active");
-        $('.nav-list').hide(); 
-      };
-
-      if (($('.nav-list').is(':visible'))) {
-        hamDiv.classList.add("is-active");
-      };
-      // if the nav-list doesn't show when ham is active
-      if (($(window).width() < 750) && (hamDiv.classList.contains("is-active") === true)){
-        $('.nav-list').show(); 
-      };  
-      // show nav in desktop
-      if ($(window).width() > 750) {
-        $('.nav-list').show(); 
-      };
-  });
+  }
 })
